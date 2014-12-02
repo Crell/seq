f1f0481fa9a835af448081e8fbc6716d45725020
Larry Garfield <larry@garfieldtech.com>
2014-12-01 23:11:28 -0600

    Test multi-line input

--- parser_test.go
+++ parser_test.go
@@ -6,17 +6,27 @@ import "bufio"
 
 func TestParse(t *testing.T) {
 
-	s := bufio.NewReader(strings.NewReader("A -> B: Test"))
+	s := bufio.NewReader(strings.NewReader("A -> B: Test\nC->D:More"))
 
 	c := make(chan *statement)
 
 	Parse(s, c)
-	x := <-c
 
+	var x *statement
+
+	x = <-c
 	if x.from != "A" {
 		t.Error("From property incorrect")
 	}
 	if x.to != "B" {
+		t.Error("To property incorrect")
+	}
+
+	x = <-c
+	if x.from != "C" {
+		t.Error("From property incorrect")
+	}
+	if x.to != "D" {
 		t.Error("To property incorrect")
 	}
 
