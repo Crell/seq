df9408b8e01cacd83d65b188ad62e23284ac3a88
Larry Garfield <larry@garfieldtech.com>
2014-12-10 23:01:29 -0600

    Switch to xsk-makepkg generated SVG rather than SVGo.

--- svgout.go
+++ svgout.go
@@ -1,54 +1,31 @@
 package seq
 
-import "github.com/ajstarks/svgo"
+import svg "github.com/metaleap/go-xsd-pkg/www.w3.org/TR/2002/WD-SVG11-20020108/SVG.xsd_go"
 import "io"
 import "fmt"
+import "encoding/xml"
+import "bytes"
 
 var _ = fmt.Printf
 
-func (d *diagram) makeSvg(out io.Writer) {
-
-	width := 500
-	height := 500
-	canvas := svg.New(out)
-	canvas.Start(width, height)
-
-	d.makeSvgParticpantDefs(canvas)
-
-//
-//	<use xlink:href="#first" x="100" y="100" />
-//	<use xlink:href="#first" x="100" y="300" />
-//	<use xlink:href="#second" x="300" y="100" />
-//	<use xlink:href="#second" x="300" y="300" />
-
-
-	canvas.End()
-	//	canvas.Circle(width/2, height/2, 100)
-	//	canvas.Text(width/2, height/2, "Hello, SVG", "text-anchor:middle;font-size:30px;fill:white")
+// Wrap the generated SVG type per instructions from xsd-makepkg
+type SvgDoc struct {
+	XMLName xml.Name `xml:"svg"`
+	svg.TsvgType
 }
 
-func (d *diagram) makeSvgParticpantDefs(canvas *svg.SVG) {
-	canvas.Def()
-	for i, participant := range d.participants {
-		nameLength := len(participant)
+func (d *diagram) makeSvg(out io.Writer) {
 
-		canvas.Group(fmt.Sprintf("id:participant-%s", i))
-		canvas.Rect(0, 0, 0, 0, fmt.Sprintf("fill:transparent;stroke:black;stroke-width:1px;height:1.5em;width:%dem", nameLength+2))
-		canvas.Text(1, 1, string(participant), "font-size:1em;text-anchor:middle")
+	img := &SvgDoc{}
 
-		canvas.Gend()
-
+	// Now render the SVG to an XML string and write it back to
+	// the provided stream.
+	output, err := xml.MarshalIndent(img, "", "  ")
+	if err != nil {
+		out.Write(bytes.NewBufferString(fmt.Sprintf("error: %v\n", err)).Bytes())
+		return
 	}
 
-	/*
-	<g id="first">
-	<rect x="1" y="1" width="5em" height="1.5em" fill="transparent" stroke="black" stroke-width="1px" />
-	<text x="1em" y="1em" style="font-size: 1em; text-anchor: middle;">Abcdef</text>
-	</g>
-	*/
-
-	// canvas.Use(x, y, "participant-1"
-
-	canvas.DefEnd()
-
+	out.Write(bytes.NewBufferString(xml.Header).Bytes())
+	out.Write(output)
 }
