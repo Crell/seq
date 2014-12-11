dce9d263dae74e5cf047620251bebf609edc07fd
Larry Garfield <larry@garfieldtech.com>
2014-12-11 00:08:41 -0600

    Generate participant group definitions.

--- svgout.go
+++ svgout.go
@@ -5,8 +5,7 @@ import "io"
 import "fmt"
 import "encoding/xml"
 import "bytes"
-
-var _ = fmt.Printf
+import xsdt "github.com/metaleap/go-xsd/types"
 
 // Wrap the generated SVG type per instructions from xsd-makepkg
 type SvgDoc struct {
@@ -18,6 +17,8 @@ func (d *diagram) makeSvg(out io.Writer) {
 
 	img := &SvgDoc{}
 
+	d.addDefs(img)
+
 	// Now render the SVG to an XML string and write it back to
 	// the provided stream.
 	output, err := xml.MarshalIndent(img, "", "  ")
@@ -28,4 +29,50 @@ func (d *diagram) makeSvg(out io.Writer) {
 
 	out.Write(bytes.NewBufferString(xml.Header).Bytes())
 	out.Write(output)
+}
+
+func (d *diagram) addDefs(img *SvgDoc) {
+	defs := &svg.TdefsType{}
+
+	for i, participant := range d.participants {
+		defs.Gs = append(defs.Gs, participantToGroupDef(i, participant))
+	}
+
+	img.Defses = append(img.Defses, defs)
+
+}
+
+/*
+  Render a participant into something roughly like this:
+	<g id="participant-2">
+	  <rect x="1" y="1" width="5em" height="1.5em" fill="transparent" stroke="black" stroke-width="1px" />
+	  <text x="1em" y="1em" style="font-size: 1em; text-anchor: middle;">Abcdef</text>
+	</g>
+*/
+func participantToGroupDef(i int, participant participant) *svg.TgType {
+	group := &svg.TgType{}
+	group.Id = xsdt.Id(fmt.Sprintf("participant-%d", i))
+
+	// I don't know why we can't declare these in the struct. The compiler
+	// says undefined field? Something quirky with the heavy nesting,
+	// maybe?
+	rect := &svg.TrectType{}
+	rect.X = "1"
+	rect.Y = "1"
+	rect.Height = "1.5em"
+	rect.Fill = "transparent"
+	rect.Stroke = "black"
+	rect.StrokeWidth = "1px"
+	rect.Width = svg.TLengthType(fmt.Sprintf("%dem", len(participant)+2))
+
+	group.Rects = append(group.Rects, rect)
+
+	text := &svg.TtextType{}
+	text.X = "1em"
+	text.Y = "1em"
+	text.Style = "font-size: 1em; text-anchor: middle"
+	text.XsdGoPkgCDATA = string(participant)
+	group.Texts = append(group.Texts, text)
+
+	return group
 }
